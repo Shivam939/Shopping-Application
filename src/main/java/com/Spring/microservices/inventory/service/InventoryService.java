624f177d57623cfa20ebc9aaee817cb579461122
Shivam Tyagi <shivtyagi939@gmail.com>
2025-03-12 16:25:45 +0530

    Flyway Migration Scripts ADDED

---
+++ src/main/java/com/Spring/microservices/inventory/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.Spring.microservices.inventory.service;
+
+import com.Spring.microservices.inventory.repository.InventoryRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
---
+++ src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES (100, 'Iphone_15'),
+       (100, 'Samsung_S20'),
+       (100, 'One_plus_7T');
