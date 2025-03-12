01839e0dc6ac66add5c22b97110849d6b914f577
Shivam Tyagi <shivtyagi939@gmail.com>
2025-03-12 23:14:40 +0530

    Inter-Service(Synchronous) Communication using Spring Cloud Feign Client

--- src/main/java/com/Spring/microservices/order/service/OrderService.java
+++ src/main/java/com/Spring/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.Spring.microservices.order.service;
 
+import com.Spring.microservices.order.client.InventoryClient;
 import com.Spring.microservices.order.dto.OrderRequest;
 import com.Spring.microservices.order.model.Order;
 import com.Spring.microservices.order.repository.OrderRepository;
@@ -13,16 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.getPrice());
-        order.setSkuCode(orderRequest.getSkuCode());
-        order.setQuantity(orderRequest.getQuantity());
+        var isProductInStock = inventoryClient.isInstock(orderRequest.getSkuCode(),orderRequest.getQuantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.getPrice());
+            order.setSkuCode(orderRequest.getSkuCode());
+            order.setQuantity(orderRequest.getQuantity());
 
-        orderRepository.save(order);
-
+            orderRepository.save(order);
+        }
+        else {throw new RuntimeException("Product with skuCode"+orderRequest.getSkuCode()+"is not in Stock");
+    }
     }
 }
--- src/test/java/com/Spring/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/Spring/microservices/order/OrderServiceApplicationTests.java
@@ -1,4 +1,6 @@
 package com.Spring.microservices.order;
+import com.Spring.microservices.order.client.InventoryClient;
+import com.Spring.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,6 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -40,6 +44,7 @@ class OrderServiceApplicationTests {
                 }
                 """;
 
+		InventoryClientStub.stubInventoryCall("iphone_15",1);
 
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
@@ -55,4 +60,3 @@ class OrderServiceApplicationTests {
 		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 }
-
