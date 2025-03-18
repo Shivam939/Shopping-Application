22949cc30e2d562a2e81a31da2817623a2ed1166
Shivam Tyagi <shivtyagi939@gmail.com>
2025-03-18 16:27:47 +0530

    Documented REST API with Open API

--- src/test/java/com/Spring/microservices/inventory/InventoryServiceApplicationTests.java
+++ src/test/java/com/Spring/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,60 +1,15 @@
 package com.Spring.microservices.inventory;
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.context.annotation.Import;
 
-
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
 class InventoryServiceApplicationTests {
 
-    @ServiceConnection
-    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
-    @LocalServerPort
-    private Integer port;
-
-    @BeforeEach
-    void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = port;
-    }
-
-    static {
-        mySQLContainer.start();
-    }
-
-    @Test
-    void shouldReadInventory() {
-        var response = RestAssured.given()
-                .when()
-                .get("/api/inventory?skuCode=iphone_15&quantity=1")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().response().as(Boolean.class);
-        assertTrue(response);
-
-        var negativeResponse = RestAssured.given()
-                .when()
-                .get("/api/inventory?skuCode=iphone_15&quantity=1000")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().response().as(Boolean.class);
-        assertFalse(negativeResponse);
-
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
-
-
-
-
-
-
